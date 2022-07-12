49ee074b203980b9021c5ba424577e9dcc666d98
Kyle <ktvitale7@gmail.com>
2022-07-11 20:21:23 -0400

    first commit

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
-function fetchPokemon(){
+function fetchPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(res => res.json())
-    .then(function(pokemon){
-        pokemon.results.forEach(function(poke){
-            pokemonData(poke)
+        .then(res => res.json())
+        .then(function (pokemon) {
+            pokemon.results.forEach(function (poke) {
+                pokemonData(poke)
+            })
         })
+}
+
+
+function pokemonData(pokemon) {
+    const url = pokemon.url
+    fetch(url)
+        .then(response => response.json())
+        .then(function (pokemon) {
+            console.log(pokemon)
+            pokemonCard(pokemon)
+        })
+}
+fetchPokemon()
+
+
+function pokemonTypes(types, ul) {
+    types.forEach(function (type) {
+        const li = document.createElement('li');
+        li.innerText = type['type']['name'];
+        ul.append(li)
     })
 }
 
 
-function pokemonData(single){
-    let url = single.url 
-      fetch(url)
-      .then(response => response.json())
-      .then(function(pokemon){
-      console.log(pokemon)
-      })
-    }
-fetchPokemon()
+
+function pokemonCard(pokeData) {
+    const pokemonPack = document.getElementById('poke-container');
+    const pokemonContainer = document.createElement("div")
+    const pokemonName = document.createElement('h4')
+    pokemonName.innerText = pokeData.name
+    const pokemonNumber = document.createElement('p')
+    pokemonNumber.innerText = `#${pokeData.id}`
+    const pTypes = document.createElement('ul')
+    const img = pokeData.sprites.front_default
+    const shinyImg = pokeData.sprites.front_shiny
+    const finalImg = `<img src="${img}" data-img="${shinyImg}">`
+    const btn = document.createElement('button')
+    btn.className = 'add-btn'
+    btn.textContent = 'Add Pokemon To Party!'
+    btn.addEventListener('click', (e) => {
+
+    })
+    const likeHearts = document.createElement('button')
+    likeHearts.className = 'like-btn'
+    likeHearts.textContent = ' ♡ '
+
+    pokemonTypes(pokeData.types, pTypes)
+    pokemonContainer.append(pokemonName, pokemonNumber);
+    pokemonContainer.innerHTML += finalImg;
+    pokemonContainer.append(pTypes, btn,);
+    pokemonContainer.append(likeHearts);
+    pokemonPack.appendChild(pokemonContainer);
 
 
 
-function createPokeCard(data){
-    
-    const img = createElement('img')
-    img.src = data.sprites.front_default
-    img.setAttribute('data-shiny', data.sprites.front_shiny)
-    img.className = 'poke-avatar'
-
-   const h2 = document.createElement('h2')
-   h2.textContent = data.name
-
-   const btn = document.createElement('button')
-   btn.id = ('id', data.id)
-   btn.className = 'add-btn'
-   btn.textContent = 'Add Pokemon'
-   btn.addEventListener('click', (e) => {
-
-   })
-
-   const pokeCard = document.querySelector('.poke-container')
-   div.append(img, h2, btn)
-   pokeCard.append(div)
 }
 
-
-
-
-
-
-//    function fetchPoke(data){
-//        const img = createElement('img')
-//    }
