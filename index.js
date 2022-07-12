bb652c1606537121062d13ddd099d58870be952f
Kyle <ktvitale7@gmail.com>
2022-07-12 10:57:48 -0400

    Created function with fetch request to get pokemon data

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-
+function fetchPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(res => res.json())
+        .then(function (pokemon) {
+            pokemon.results.forEach(function (poke) {
+                pokemonData(poke)
+            })
+        })
+}
 
 
